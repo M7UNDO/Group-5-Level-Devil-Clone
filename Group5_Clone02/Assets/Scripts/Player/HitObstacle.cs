9f24b19c0c1b58edda04e70b5c819018e6ad464e
M7UNDO <dhlaminimfundo1@gmail.com>
2025-08-12 22:29:58 +0200

    Player Death/Respawn with particle effect

---
+++ Group5_Clone02/Assets/Scripts/Player/HitObstacle.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using UnityEngine;
+
+public class HitObstacle : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private ParticleSystem particleEffectPrefab;
+    [SerializeField] private Transform respawnPoint;
+    [SerializeField] private float respawnDelay = 2f;
+
+    [SerializeField] private SpriteRenderer[] allPlayerRenderers;
+    private PlayerMovement playerMovement;
+    private Rigidbody2D rb;
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        playerMovement = player.GetComponent<PlayerMovement>();
+        rb = player.GetComponent<Rigidbody2D>();
+
+        startPosition = player.transform.position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D coli)
+    {
+        if (coli.gameObject.CompareTag("Spikes"))
+        {
+            StartCoroutine(HandleDeath());
+        }
+    }
+
+    private IEnumerator HandleDeath()
+    {
+        ParticleSystem effect = Instantiate(particleEffectPrefab, player.transform.position, Quaternion.identity);
+        effect.Play();
+
+        if (playerMovement != null) playerMovement.enabled = false;
+        foreach(SpriteRenderer sprite in allPlayerRenderers)
+        {
+            sprite.enabled = false;
+        }
+
+
+        rb.velocity = Vector2.zero;
+        RigidbodyConstraints2D originalConstraints = rb.constraints;
+        rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+
+        yield return new WaitForSeconds(respawnDelay);
+
+        // Respawn position
+        Vector3 spawnPos = respawnPoint != null ? respawnPoint.position : startPosition;
+        player.transform.position = spawnPos;
+
+        rb.constraints = originalConstraints;
+
+        foreach (SpriteRenderer sprite in allPlayerRenderers)
+        {
+            sprite.enabled = true;
+        }
+        if (playerMovement != null) playerMovement.enabled = true;
+        Destroy(effect.gameObject, effect.main.duration);
+    }
+}
